8d297aa2ac22eff6af8ec90234a14ac9ee94b3f5
Andrew Pryde <andrew@pryde-design.co.uk>
2011-08-12 17:23:02 +0100

    Fix for lack of hover on remotely loaded images

--- greyScale.js
+++ greyScale.js
@@ -82,7 +82,7 @@
             url: $(this).attr('src'),
             success: $.proxy(function(image) {
                 can = greyScale(image, image.width, image.height);
-                if ($options.reverse) { can.appendTo(gsWrapper); }
+                if ($options.reverse) { can.appendTo(gsWrapper).css({"display" : "block", "opacity" : "0"}); }
                 else { can.appendTo(gsWrapper).fadeIn($options.fadeTime); }
               }, gsWrapper),
             error: function(xhr, text_status){
